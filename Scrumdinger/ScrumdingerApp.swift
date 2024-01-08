b3a63a4993d201ffa1f626da413aab1586c4122c
ahmetkasimnazli <ahmetnazl2@gmail.com>
2024-01-08 16:02:02 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ahmet Kasım Nazlı on 8.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
